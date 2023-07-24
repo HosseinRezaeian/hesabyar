4dfadde67e94a87ae21f53dae90f3604052d01d7
HosseinRezaeian <hossainrezaeian@gmail.com>
2023-07-24 20:10:23 +0330

    add bank account

---
+++ apps/Bank_Account/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Bank_Account
+
+
+# Register your models here.
+
+@admin.register(Bank_Account)
+class BankAccountAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'accountNumber', 'cash')  # Fields to display in the list view
+    search_fields = ('user__username', 'name', 'accountNumber')  # Add fields for searching
+    list_filter = ('user',)
---
+++ apps/Bank_Account/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+
+
+# Create your models here.
+class Bank_Account(models.Model):
+    account_number_validator = RegexValidator(
+        regex=r'^\d{4}-\d{4}-\d{4}-\d{4}$',
+        message='Enter a valid account number in the format XXXX-XXXX-XXXX-XXXX.'
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    accountNumber = models.CharField(max_length=19,  # Set the maximum length for the formatted account number
+                                     validators=[account_number_validator])  # Replace with your desired minimum value
+
+    cash = models.IntegerField()
+
+    def clean(self):
+        # Remove any hyphens from the input and check the format
+        cleaned_account_number = self.accountNumber.replace('-', '')
+        if not cleaned_account_number.isdigit() or len(cleaned_account_number) != 16:
+            raise ValidationError('Enter a valid 16-digit account number.')
+
+        # Add the hyphens to the account number in the desired format
+        formatted_account_number = '-'.join(
+            cleaned_account_number[i:i + 4] for i in range(0, 16, 4)
+        )
+
+        self.accountNumber = formatted_account_number
+
+    def __str__(self):
+        return f"{self.user.username}'s Bank Account - {self.accountNumber}"
